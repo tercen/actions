6373c9da07931b007e199aea7b8e6d584372fde8
Thiago Monteiro <tsmonteiro@gmail.com>
2024-02-01 10:41:58 -0300

    less verbose on fail

--- start-tercen/check-tercen-status.sh
+++ start-tercen/check-tercen-status.sh
@@ -9,15 +9,14 @@ do
     docker run --net=host --rm -e TERCEN_USERNAME=test \
     				-e TERCEN_PASSWORD=test \
     				-e TERCEN_URI=$1:$2  \
-    				tercen/tercenctl:release session  
+    				tercen/tercenctl:release session  > /dev/null
     STATUS=$?
     
     if (( STATUS > 0 )); then
-    	# No one is listening
+        echo "Connecting to $1:$2 unsuccessful ... Retrying in 10 seconds [$TRY/30]"
         sleep 10
     else
         echo "Listen to $1:$2 successful"
         break
     fi
 done
-
